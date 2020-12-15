eb4102f4925674f1c9590de78ec792b27008cc84
Donaldo Ayala <ayala.segoviano.donaldo@gmail.com>
2020-12-14 18:08:07 -0600

    Changed 'Terminar' button's functionality to just hide when clicked instead of closing the app

--- app/src/main/java/com/example/pickingapp/MainActivity.java
+++ app/src/main/java/com/example/pickingapp/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }).setNegativeButton("Terminar", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        finish();
+                        
                     }
                 });
                 AlertDialog dialog = builder.create();
